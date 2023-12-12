0d573e5b14b26b145c0946e47b1cc9ce2c697993
Kartikey Sapkal <130264100+KartikeySapkal@users.noreply.github.com>
2023-12-12 11:39:48 +0530

    Add files via upload

--- leftRotation.cpp
+++ leftRotation.cpp
@@ -7,24 +7,27 @@ int main(){
     cin>>k;
     
     
-    int n=10;
+    int n=5;
     int arr[n+k];
     for(int i=0; i<n; i++){ 
     cin>>arr[i];   
     }
         
     // cout<<arr[1];
-
-    for(int i=9; i>=0; i--){ 
+    int count=0;
+    for(int i=n-1; i>=0; i--){ 
         arr[i+k] = arr[i];
+        count++;
     }
     for(int i=0; i<k; i++){ 
-        arr[i] = arr[n+i] ;   
+        arr[i] = arr[n+i] ;
+        count++;
     }
 
-    for(int i=0; i<10; i++){ 
+    for(int i=0; i<n; i++){ 
         cout<<arr[i]<<" ";
     }
+    cout<<count<<endl;
 
 
     return 0;
